12b1fe225f88eaffce97514906021df2f6ad2650
nekk <kunkaad@gmail.com>
2020-08-28 11:01:14 +0300

    betterRes

--- task_1/Task_1.js
+++ task_1/Task_1.js
@@ -1,32 +1,23 @@
-const quadratic_equation_res = (array_of_abc = [1,0,-4]) => {
-    a = array_of_abc[0] 
-    b = array_of_abc[1] 
-    c = array_of_abc[2]
-    const Discriminant = b*b-4*a*c
-    let x1 = (-b + Math.sqrt(Discriminant)/ (2*a))
-    let x2 = (-b - Math.sqrt(Discriminant)/ (2*a))
-
-
-    if (Array.isArray(array_of_abc) && array_of_abc.length == 3){
-
-        if(a==0) return console.log(`Ощибка a - должно быть > 0 или < 0 `);
-
-        if(Discriminant < 0){
-            return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорней нет!`)
+const quadraticEquationRes = ([a,b,c]) => {
     
-        } 
-        else if(Discriminant==0){
-            x1 = -b/(2*a)
-            return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорень уравнения: ${x1}`)
-        }
-        else if(Discriminant >0){
-            return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорни уравнения: ${x1} , ${x2}`)
-        }
+    const discriminant = b*b-4*a*c
+
+    if(a==0) return console.log(`Ощибка a - должно быть > 0 или < 0 `);
+
+    if(discriminant < 0){
+        return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорней нет!`)
     
-    }   
-    else{
-        return console.log(`Функция принимает только массив из 3х элементов!!!`);
+    } 
+    else if(discriminant==0){
+        x1 = -b/(2*a)
+        return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорень уравнения: ${x1}`)
     }
+    else if(discriminant >0){
+        let x1 = (-b + Math.sqrt(discriminant)/ (2*a))
+        let x2 = (-b - Math.sqrt(discriminant)/ (2*a))
+        return console.log(`Уравнение имеет вид: ${a}x + ${b}x + ${c} = 0 \nКорни уравнения: ${x1} , ${x2}`)
+    }
+    
+}   
 
-}
-///
+
